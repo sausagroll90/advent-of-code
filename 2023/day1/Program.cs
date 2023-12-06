8d9b06e15a5e8f7675adc14e2ae925fc990de0c2
Dominic Francis <dominicf09@gmail.com>
2023-12-06 00:01:48 +0000

    Add project files.

---
+++ 2023/day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+string[] input = File.ReadAllLines("input.txt");
+
+int valueTotal = 0;
+
+foreach (string line in input)
+{
+    bool first = true;
+    char firstDigit = new char();
+    char lastDigit = new char();
+    foreach (char c in line) {
+        if (char.IsDigit(c)) {
+            lastDigit = c;
+            if (first) {
+                firstDigit = c;
+                first = false;
+            }
+        }
+    }
+    char[] chars = { firstDigit, lastDigit };
+    string valueLineString = new string(chars);
+    int valueLine = Int32.Parse(valueLineString);
+    valueTotal += valueLine;
+}
+
+Console.WriteLine(valueTotal);
